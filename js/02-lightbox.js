5710350a04255c64e9f9f406490615ec87fb36c0
Reut Mihai <reutmihai@icloud.com>
2024-08-17 18:05:29 +0300

    exercise 2 solved

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+function createGalleryMarkup(items){
+    return items.map(item => {
+        const { preview, original, description } = item;
+        return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+        </li>`;
+    }).join('');
+}
+
+const renderGallery = () => {
+    galleryList.innerHTML = createGalleryMarkup(galleryItems);
+} 
+
+renderGallery();
+
+const lightbox = new 
+SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: 'bottom',
+  enableKeyboard: true,
+});
